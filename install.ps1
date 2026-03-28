9947bfa0b9bf8adb32c61af5187b0723fe33e4d1
ah5e <wwfawaz220@gmail.com>
2026-03-28 10:26:27 +0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,6 +1,6 @@
 $zip = "$env:TEMP\velaris.zip"
 $folder = "$env:USERPROFILE\Desktop\VELARIS"
 
-Invoke-WebRequest "https://raw.githubusercontent.com/ah5e/Velaris2/main/velaris.zip" -OutFile $zip
+Invoke-WebRequest "https://raw.githubusercontent.com/ah5e/Velaris2/main/VELARIS%20Premium.zip" -OutFile $zip
 Expand-Archive -Path $zip -DestinationPath $folder -Force
 Start-Process "$folder\VELARIS Premium.exe"
